26a4844bb33b0b3ce190eb3bb74a09c559a4563a
KobNoi <59143425@g.cmru.ac.th>
2019-07-13 14:14:58 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => res.send('Hello Jimi! I feel Happy'))
-app.get('/about', (req, res) => res.send('เปิดมาทำไหม เสือก'))
 app.get('/contec', (req, res) => res.send('ยุ่งไรกับกู'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
